7707f11b07eedbfb3da3c43401a23bd7a1caa638
jedithedev <77647468+jedithedev@users.noreply.github.com>
2023-05-09 07:23:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from discord import SyncWebhook
+
+app = Flask(__name__)
+
+
+@app.route('/api/webhooks/<data1>/<data2>', methods=['GET', 'POST'])
+def index(data1, data2):
+  if request.method == 'POST':
+    print(request.get_json())
+    #webhook=SyncWebhook.from_url(request.get_json()['url'])
+    webhook=SyncWebhook.from_url('https://discord.com/api/webhooks/'+data1+'/'+data2)
+    webhook.send(request.get_json()['content'])
+    return ''
+  return 'Error, get methods cannot be used here'
+
+
+app.run(host='0.0.0.0', port=81)
